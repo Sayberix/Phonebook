8e9b3efb78b1fb23d4fa8a4ee8474646b2d75669
Sayberix <mr.maaks@gmail.com>
2023-01-03 23:07:21 +0700

    добавлена функция 'add_element'

--- data_base.py
+++ data_base.py
@@ -1,1 +1,12 @@
+import os
+from pathlib import Path
 
+def add_element(data: str, position: int):
+    position = 0
+    data = 'Konev'
+    name_file = '/db/db'
+    file_path = Path(os.getcwd() + name_file, os.getcwd() + name_file + '.txt')
+    with open(file_path, 'a', encoding="utf-8") as data_write:
+        data_write.write(f'\n{data}')
+
+add_element('Konev',0)
