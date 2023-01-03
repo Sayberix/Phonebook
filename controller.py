b839dd5c4380ba6328af4dc7d72f3154d4338832
Sayberix <mr.maaks@gmail.com>
2023-01-04 00:04:47 +0700

    добавлена функция splitlines() при считывании строк, удаляет символы переноса строки

--- controller.py
+++ controller.py
@@ -1,1 +1,10 @@
+import data_base as db
 
+name_file = '/db/db'
+
+list_phone = []
+list_phone2 = []
+list_phone = db.read_element(name_file)
+list_phone2 = list_phone[0].split(';')
+print(list_phone)
+print(list_phone2)
--- data_base.py
+++ data_base.py
@@ -9,10 +9,10 @@ def add_element(data: str, name_file: str):
     with open(file_path, 'a', encoding="utf-8") as data_write:
         data_write.write(f'\n{data}')
 
-def read_element(name_file: str):
+def read_element(name_file: str) -> str:
     file_path = Path(getcwd() + name_file, getcwd() + name_file + '.txt')
     with open(file_path, 'r', encoding="utf-8") as data_read:
-        return data_read.readlines()
+        return data_read.read().splitlines()
 
 #add_element('Konev', name_file)
-print(read_element(name_file))
+#print(read_element(name_file))
