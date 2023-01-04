3bf1a2eececb19a41172400597e25a0a3981227b
Sayberix <mr.maaks@gmail.com>
2023-01-04 17:11:32 +0700

    добавлена ф-ция get_element_list

--- controller.py
+++ controller.py
@@ -4,12 +4,11 @@
 import data_base as db
 from parameters import name_file
 
-#name_file = '/db/db'
-
 list_phone = []
-list_phone2 = []
 list_phone = db.read_element(name_file)
-list_phone2 = list_phone[0].split(';')
+
+def get_element_list(list_phone: list, position: int) -> list:
+    return list_phone[0].split(';')
+
 print(list_phone)
 print(list_phone2)
-# пока без функции
