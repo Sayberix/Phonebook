58185ff1981cd476af20ee0e5db08fc58551f172
Sayberix <mr.maaks@gmail.com>
2023-01-06 19:21:53 +0700

    переименовал ф-цию с add_element в write_element, добавил аргумент по типу операции

--- data_base.py
+++ data_base.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from parameters import name_file
 
-def add_element(data: str, name_file: str):
+def write_element(op: str, data: str, name_file: str):
     file_path = Path(name_file, name_file + '.txt')
-    with open(file_path, 'a', encoding="utf-8") as data_write:
+    with open(file_path, op, encoding="utf-8") as data_write:
         data_write.write(f'\n{data}')
 
 def read_element(name_file: str) -> str:
