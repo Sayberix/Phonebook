f114acc05a0dd35b4c551b941ac3f377269a3efb
Sayberix <mr.maaks@gmail.com>
2023-01-07 00:03:58 +0700

    реализована ф-ция ui()

--- user_interface.py
+++ user_interface.py
@@ -1,15 +1,30 @@
+import sys
 import controller as cntrl
 
-print("         Телефонный справочник:")
-print("Режимы работы с телефонным справочником:")
-print("1. Просмотр всего справочника")
-print("2. Поиск по реквизитам")
-print("3. Добавление данных")
-print("4. Удаление данных")
-print("5. Выход из справочника")
-n = int(input("Выберите режим работы с телефонным справочником: "))
+def ui():
+    print("         Телефонный справочник:")
+    print("Режимы работы с телефонным справочником:")
+    print("1. Просмотр всего справочника")
+    print("2. Поиск по реквизитам")
+    print("3. Добавление данных")
+    print("4. Редактирование данных")
+    print("5. Удаление данных")
+    print("6. Выход из справочника")
 
-match n:
-    case 1:
-        cntrl.get_all_element_list()
-    case 2:
+    n = int(input("Выберите режим работы с телефонным справочником: "))
+
+    match n:
+        case 1:
+            cntrl.view_all_list()
+        case 2:
+            cntrl.find_record_in_list()
+        case 3:
+            cntrl.add_record_in_list()
+        case 4:
+            cntrl.editing_record_in_list()
+        case 5:
+            cntrl.delete_record_in_list()
+        case 6:
+            sys.exit()
+        case _:
+            ui()
