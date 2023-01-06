45341d8e696f11d9008c10fef494257f67364b70
Sayberix <mr.maaks@gmail.com>
2023-01-06 23:28:07 +0700

    убрал с аргументов ф-ций переменную list_phone_func, объявил внутри ф-ций через глобальную переменную list_phone

--- controller.py
+++ controller.py
@@ -7,30 +7,35 @@ from parameters import name_file
 list_phone = []
 list_phone = db.read_element(name_file)
 
-# получение записи строки в виде списка
-def get_element_list(list_phone_func: list, position_in_list: int) -> list:
-    return list_phone_func[position_in_list].split(';')
+# получение записи строки в виде списка (внутренняя ф-ция)
+def get_element_list(position_in_list: int) -> list:
+    global list_phone
+    return list_phone[position_in_list].split(';')
 
+# шапка колонок
 def column_header():
     return ['Фамилия','Имя','Телефон','Описание']
 
 # представление в удобочитаемой форме
-def view_list(list_phone_func: list):
-    [print(list_phone_func[i], ' | ', end=' ') if i < len(list_phone_func) - 1 else print(list_phone_func[i], ' | ', '\n') for i in range(len(list_phone_func)) ]
+def view_list():
+    global list_phone
+    [print(list_phone[i], ' | ', end=' ') if i < len(list_phone) - 1 else print(list_phone[i], ' | ', '\n') for i in range(len(list_phone)) ]
         
 # просмотр всех записей
-def view_all_list(list_phone_func: list):
-    [view_list(get_element_list(list_phone_func,i)) for i in range(len(list_phone_func))]
+def view_all_list():
+    global list_phone
+    [view_list(get_element_list(list_phone,i)) for i in range(len(list_phone))]
 
 # добавление записей в строку
 def add_record_in_list() -> str:
     return str(input('Введите фамилию: ')) + ';' + str(input('Введите имя: ')) + ';' + str(input('Введите телефон: ')) + ';' + str(input('Введите описание: ')) + '\n'
 
 # удаление записи по ключевому слову
-def delete_record_in_list(list_phone_func: list, text_word: str) -> str:
+def delete_record_in_list(text_word: str) -> str:
+    global list_phone
     resault = ''
-    for i in range(len(list_phone_func)):
-        entry = get_element_list(list_phone_func,i)
+    for i in range(len(list_phone)):
+        entry = get_element_list(list_phone,i)
         for j in entry:
             find_word = False
             if text_word in j:
@@ -46,10 +51,11 @@ def delete_record_in_list(list_phone_func: list, text_word: str) -> str:
     db.write_element('w', resault, name_file)
 
 # редактирование записи по ключевому слову
-def editing_record_in_list(list_phone_func: list, text_word: str) -> str:
+def editing_record_in_list(text_word: str) -> str:
+    global list_phone
     resault = ''
-    for i in range(len(list_phone_func)):
-        entry = get_element_list(list_phone_func,i)
+    for i in range(len(list_phone)):
+        entry = get_element_list(list_phone,i)
         for j in entry:
             find_word = False
             if text_word in j:
@@ -66,10 +72,11 @@ def editing_record_in_list(list_phone_func: list, text_word: str) -> str:
     db.write_element('w', resault, name_file)
 
 # поиск слова по всему списку справочника и запись его позиции в список кортежей
-def find_word_in_list(list_phone_func: list, text_word: str) -> list[tuple]:
+def find_word_in_list(text_word: str) -> list[tuple]:
+    global list_phone
     resault = []
-    for i in range(len(list_phone_func)):
-        entry = get_element_list(list_phone_func,i)
+    for i in range(len(list_phone)):
+        entry = get_element_list(list_phone,i)
         for j in range(len(entry)):
             if entry[j] == text_word:
                 tuple_rec = (i, j)  # где i - это номер строки а j - номер записи в строке
