0e7530783de4f4bbbd3ce141f6597ed5292c08fa
Sayberix <mr.maaks@gmail.com>
2023-01-07 13:55:15 +0700

    добавил файл лога в репозиторий

---
+++ logger.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+import data_base as db
+from datetime import datetime as dt
+from time import timezone
+from parameters import name_file_log
+
+ # ф-ция логирования просмотра всех записей, можно добавить счетчик просмотренных записей
+def log_view_all_record():
+    global name_file_log
+    data = ''
+    time = dt.now().strftime('%H:%M') + ";"
+    data = time + "view all records" + "\n"
+    db.write_element('a', data, name_file_log)
+
+# ф-ция логирования добавления записи
+def log_add_record(data):
+    global name_file_log
+    time = dt.now().strftime('%H:%M') + ";"
+    data = time + "adding an entry: " + data + "\n"
+    db.write_element('a', data, name_file_log)
+
+# ф-ция логирования удаления записи
+def log_delete_record(data):
+    global name_file_log
+    time = dt.now().strftime('%H:%M') + ";"
+    data = time + "deleting an entry: " + data + "\n"
+    db.write_element('a', data, name_file_log)
+
+# ф-ция логирования редактирования записи
+def log_update_record(data):
+    global name_file_log
+    time = dt.now().strftime('%H:%M') + ";"
+    data = time + "record editing: " + data + "\n"
+    db.write_element('a', data, name_file_log)
